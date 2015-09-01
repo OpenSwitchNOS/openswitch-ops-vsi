d02ceb043a012a5e8073d2ccd11f4e2ce2ba7717
Sreedhar <sreedharr@hp.com>
2015-09-01 04:29:39 -0500

    Renaming HalonVsi to OpsVsi.
    For time being the existing code is copied to OpsVsi.
    Once all the tests are moved to the new naming
    convention old HalonVsi files will be removed.
    
    Change-Id: Ibfd37ca220641e2f75037a58421ad39ebf3c5037
    Signed-off-by: Sreedhar <sreedharr@hp.com>

---
+++ opsvsi/docker.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+
+from mininet.net import *
+from mininet.node import *
+from mininet.link import *
+from mininet.cli import *
+from mininet.log import *
+from mininet.util import *
+from subprocess import *
+import select
+
+# Using this constant for init_cmd will allow the docker image to execute
+# its own startup scripts/default CMD as defined in its corresponding Dockerfile.
+DOCKER_DEFAULT_CMD = "DOCKER_DEFAULT_CMD"
+
+class DockerNode(Node):
+    def __init__(self, name, image='ubuntu', **kwargs):
+        self.image = image
+
+        self.testid = kwargs.pop('testid', None)
+        self.container_name = self.testid + '_' + name
+
+        self.testdir = kwargs.pop('testdir', None)
+        self.nodedir = self.testdir + '/' + name
+        os.makedirs(self.nodedir)
+
+        self.shareddir = self.nodedir + '/shared'
+        os.makedirs(self.shareddir)
+
+        self.mounts = kwargs.pop('mounts', [])
+        self.init_cmd = kwargs.pop('init_cmd', '/sbin/init')
+
+        self.nodetype = kwargs.pop('nodetype', "VsiOpenSwitch")
+
+        # Just in case test isn't running in a container,
+        # clean up any mess left by previous run
+        call( [ "docker rm -f " + self.container_name ], stdout=PIPE,
+                stderr=PIPE, shell=True)
+
+        mountParams = []
+        for mount in self.mounts:
+            mountParams += ["-v", mount]
+
+        # If OpsVsiHost simulate terminal, and run BASH
+        if self.nodetype == "OpsVsiHost":
+            dockerOptions = "-dt"
+            self.init_cmd = "/bin/bash"
+        else:
+            dockerOptions = "-d"
+
+        self.bashrc_file_name = "mininet_bash_rc"
+        f = open(self.shareddir + '/' + self.bashrc_file_name, "w")
+        f.write("export PS1='\177'")
+        f.close()
+
+        # /tmp File system on the docker app is wiped out after starting the docker.
+        # So don't create any files in /tmp directory of the docker app.
+        cmd = ["docker", "run", "--privileged",
+              "-v", self.shareddir + ":/shared",
+              "-v", "/dev/log:/dev/log",
+              "-v", "/sys/fs/cgroup:/sys/fs/cgroup"] + \
+              mountParams + \
+              ["-h", self.container_name, "--name=" + self.container_name, dockerOptions,
+               self.image]
+
+        if self.init_cmd != DOCKER_DEFAULT_CMD:
+            cmd.append(self.init_cmd)
+
+        Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+
+        # Wait until container actually starts and grab it's PID
+        while True:
+            pid_cmd = [ "docker", "inspect", "--format='{{ .State.Pid }}'",
+                        self.container_name]
+            pidp = Popen(pid_cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
+                         close_fds=False)
+            ps_out = pidp.stdout.readlines()
+            pidp.wait()
+            if pidp.returncode == 0:
+                pid = int(ps_out[0])
+                if pid != 0:
+                    self.docker_pid = pid
+                    debug("Docker container started.\n")
+                    debug(" Name=" + self.container_name);
+                    debug(" PID=", self.docker_pid);
+                    break
+
+        super(DockerNode, self).__init__(name, **kwargs)
+
+    def popen(self, *args, **kwargs):
+        return Node.popen(self, *args, mncmd=['docker', 'exec',
+                          self.container_name],
+                          **kwargs)
+
+    def terminate(self):
+        if self.shell:
+            call(["docker rm -f "+self.container_name], stdout=PIPE,
+                 stderr=PIPE, shell=True)
+            self.cleanup()
+
+    def startShell(self):
+        if self.shell:
+            error("%s: shell is already running")
+            return
+
+        cmd = [ "mnexec", "-cd", "script", "-c",
+                    ' '.join( [ "docker", "exec", "-ti", self.container_name,
+                                "/bin/bash", "--rcfile", "/shared/" + self.bashrc_file_name]),
+                    "--timing=" + self.nodedir + "/transcript.timing",
+                    "-q", "-f", self.nodedir + "/transcript"]
+
+        self.shell = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
+                           close_fds=True)
+        self.stdin = self.shell.stdin
+        self.stdout = self.shell.stdout
+        self.pid = self.docker_pid
+        self.pollOut = select.poll()
+        self.pollOut.register(self.stdout)
+        self.outToNode[self.stdout.fileno()] = self
+        self.inToNode[self.stdin.fileno()] = self
+        self.execed = False
+        self.lastCmd = None
+        self.lastPid = None
+        self.readbuf = ''
+        self.waiting = False
+
+        # Wait for prompt
+        while True:
+            data = self.read(1024)
+            if data[-1] == chr(127):
+                break
+            self.pollOut.poll()
+        self.waiting = False
+        # +m: disable job control notification
+        self.cmd('unset HISTFILE; stty -echo; set +m')
+
+
+class DockerHost (DockerNode):
+    pass
+
+
+class DockerLink(Link):
+    def __init__(self, node1, node2, **kwargs):
+        super(DockerLink, self).__init__(node1, node2, **kwargs)
+
+    def makeIntfPair(cls, intfname1, intfname2, addr1=None, addr2=None,
+                     node1=None, node2=None, deleteIntfs=True):
+
+        node1_netns = " netns " + str(node1.pid) if node1.inNamespace else " "
+        node1_netns_exec = "ip netns exec " + \
+                           str(node1.pid) if node1.inNamespace else ""
+
+        node2_netns = " netns " + str(node2.pid) if node2.inNamespace else " "
+        node2_netns_exec = "ip netns exec " + \
+                           str(node2.pid) if node2.inNamespace else ""
+
+        if deleteIntfs:
+            call([node1_netns_exec + " ip link del " + intfname1],
+                 stdout=PIPE, shell=True)
+            call([node2_netns_exec + " ip link del " + intfname2],
+                 stdout=PIPE, shell=True)
+
+        call(["ip link add " + node1_netns + " name " + intfname1 + " down " +
+              " type veth peer " + node2_netns + " name " +
+              intfname2 + " down "], shell=True)
---
+++ opsvsi/opsvsitest.py
@@ -0,0 +1,214 @@
+#!/usr/bin/python
+
+from mininet.net import *
+from mininet.node import *
+from mininet.link import *
+from mininet.cli import *
+from mininet.log import *
+from mininet.util import *
+from mininet.topo import *
+from docker import *
+from subprocess import *
+from subprocess import *
+import shutil
+import select
+import re
+import argparse
+import os
+import uuid
+
+SWNS_EXEC = '/sbin/ip netns exec swns '
+
+class OpsVsiHost (DockerHost):
+    def __init__(self, name, **kwargs):
+        kwargs['nodetype'] = "OpsVsiHost"
+
+        image = kwargs.pop('HostImage')
+        super(OpsVsiHost, self).__init__(name, image, **kwargs)
+
+
+class OpsVsiLink(DockerLink):
+    def intfName(self, node, n):
+        if isinstance(node, VsiOpenSwitch):
+            return repr(n)
+        else:
+            return super(OpsVsiLink, self).intfName(node, n)
+
+
+class VsiOpenSwitch (DockerNode, Switch):
+    def __init__(self, name, image='openswitch/genericx86-64',
+                 numPorts=54, **kwargs):
+        kwargs['nodetype'] = "VsiOpenSwitch"
+
+        # During OpenSwitch CIT test run, CT/FT tests
+        # can be run on multiple sandboxes at the same time.
+        # Each sandbox needs a different docker switch image.
+        # The top level makefile exports a ENV variable
+        # with the docker image name for that test run.
+        test_image = os.environ.get('VSI_IMAGE_NAME')
+        if test_image is not None:
+            image = test_image
+
+        # Start Openswitch firmware in a docker
+        super(VsiOpenSwitch, self).__init__(name, image, **kwargs)
+
+        self.inNamespace = True
+        self.numPorts = numPorts
+
+    def start(self, controllers):
+        # Create TUN tap interfaces.
+        # Mininet would have created as many interfaces
+        # as the number of the hosts defined in the TOPO.
+        # Create the rest as TUN TAP interfaces
+        # in 'swns' namespace.
+        for i in range(1, self.numPorts + 1):
+            if str(i) not in self.nameToIntf:
+                self.cmd(SWNS_EXEC + "/sbin/ip tuntap add dev " + str(i) + " mode tap")
+
+        # In generic-X86 image ports 49-54 are QSFP splittable ports.
+        # so create subports for them.
+        for i in irange(49, 54):
+            for j in irange(1, 4):
+                self.cmd(SWNS_EXEC + "/sbin/ip tuntap add dev " + str(i) + "-" + str(j) + " mode tap")
+
+        # Move the interfaces created by Mininet to swns namespace.
+        for intf in self.nameToIntf:
+            if intf == 'lo':
+                continue
+            self.cmd("/sbin/ip link set " + intf + " netns swns")
+            self.swns_cmd("/sbin/ip link set " + intf + " up")
+
+    def startCLI(self):
+        # The vtysh shell is opened as subprocess in the docker
+        # in interactive mode and the -t option in the vtysh adds
+        # chr(127) in the prompt which we poll for in the read.
+        cmd = ["docker","exec","-i",self.container_name, "/usr/bin/vtysh", "-t"]
+        vtysh = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+        self.cliStdin = vtysh.stdin
+        self.cliStdout = vtysh.stdout
+
+        # Wait for prompt
+        data = self.readCLI(vtysh.stdout.fileno(), 1024)
+
+    def startShell(self):
+
+        DockerNode.startShell(self)
+
+        # Wait until the OVSDB is up in the Openswitch.
+        # Copy a script which waits until 'cur_hw' in the
+        # Open_Vswitch tables becomes greater than 0
+        dir, f = os.path.split(__file__)
+        switch_wait = os.path.join(dir, "scripts", "wait_for_openswitch")
+        shutil.copy(switch_wait, self.shareddir)
+        self.cmd("/shared/wait_for_openswitch")
+
+        self.startCLI()
+
+    def writeCLI(self, fd, inp):
+        os.write(fd, inp + "\n")
+
+    def readCLI(self, fd, buflen=1024):
+        out = ''
+        while True:
+            data = os.read(fd, buflen)
+            if len(data) > 0:
+                out += data
+            if data[-1] == chr(127):
+                out.replace(chr(127),'')
+                break
+        return out
+
+    def cmdCLI(self, inp, waiting=True):
+        if waiting:
+            self.writeCLI(self.cliStdin.fileno(), inp)
+            return self.readCLI(self.cliStdout.fileno(), 1024)
+        else:
+            self.writeCLI(self.cliStdin.fileno(), inp)
+        return ''
+
+    def stop(self, deleteIntfs=True):
+        pass
+
+    def swns_cmd(self, cmd):
+        return self.cmd(SWNS_EXEC + cmd)
+
+    # OVS commands add double quotes around the strings
+    # in the output. This function removes them from the output.
+    def ovscmd(self, cmd):
+        out = self.cmd(cmd)
+
+        # Remove all the double quotes in the output.
+        out = out.replace('"', '')
+
+        # Some of the OVS commands are printing multiple new lines.
+        out = out.replace('\r\r','\r')
+
+        return out
+
+class OpsVsiTest(object):
+    def __init__(self, test_id=None, switchmounts=[], hostmounts=[], hostimage='ubuntu:latest', start_net=True):
+        # If 'test_id' is not passed create a random UUID.
+        # Docker is unable to handle a container name with complete UUID.
+        # So take only the fifth field of it.
+        if test_id is None:
+            test_id = str(uuid.uuid4().fields[4])
+            sbox_uuid = os.environ.get('SANDBOX_UUID')
+            if sbox_uuid is not None:
+                test_id = sbox_uuid + "-" + test_id
+
+        self.id = test_id
+        self.switchmounts = switchmounts
+        self.hostmounts = hostmounts
+        self.hostimage = hostimage
+
+        # Set log level to 'debug' to enable Debugging.
+        self.setLogLevel('info')
+        info("\n============= OpenSwitchVsi TEST START =============\n")
+
+        self.testdir = "/tmp/openswitch-test/" + self.id
+        shutil.rmtree(self.testdir, ignore_errors=True)
+        os.makedirs(self.testdir)
+        info("Test log dir: " + self.testdir + "\n")
+
+        # Setup and start the network topology.
+        if start_net is True:
+            self.setupNet()
+            self.net.start()
+
+    def setLogLevel(self, levelname='info'):
+        setLogLevel(levelname)
+
+    def getNodeOpts(self):
+        return {'testid': self.id, 'testdir': self.testdir}
+
+    def setHostImageOpts(self, hostimage):
+        self.hostimage = hostimage
+
+    def getHostOpts(self):
+        opts = self.getNodeOpts()
+        opts.update({'mounts':self.hostmounts})
+        opts.update({'HostImage':self.hostimage})
+        return opts
+
+    def getSwitchOpts(self):
+        opts = self.getNodeOpts()
+        opts.update({'mounts':self.switchmounts})
+        return opts
+
+    def stopNet(self):
+        self.net.stop()
+
+    def setupNet(self):
+        # If you override this function, make sure to pass
+        # Host/Switch options into hopts/sopts of the topology that
+        # you build or into addHost/addSwitch calls
+        topo = SingleSwitchTopo(k=2,
+                                hopts=self.getHostOpts(),
+                                sopts=self.getSwitchOpts())
+
+        self.net = Mininet(topo,
+                           switch=VsiOpenSwitch,
+                           host=OpsVsiHost,
+                           link=OpsVsiLink,
+                           controller=None,
+                           build=True)
